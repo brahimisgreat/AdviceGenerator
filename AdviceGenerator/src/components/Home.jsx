73c81f3f17bc8f82ee2f5fe73706b347e4ad4750
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-02-07 23:42:06 -0500

    yes

--- AdviceGenerator/src/components/Home.jsx
+++ AdviceGenerator/src/components/Home.jsx
@@ -1,19 +1,18 @@
-import './Home.css'
-import pic from '../assets/images/pause.svg'
-import dice from '../assets/images/icon-dice.svg'
+import "./Home.css";
+import pic from "../assets/images/pause.svg";
+import dice from "../assets/images/icon-dice.svg";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
-    <div id='home-container'>
-        <div id='home-kidtainer'>
-            <h3>
-                Advice #{'117'}
-                <p>{`'it is was to sit up and take notice, what's diffuclt is gettinf up and taking action`}</p>
-            </h3>
-            <button><img src={pic} /></button>
-            
-        </div>
-        
+    <div id="home-container">
+      <div id="home-kidtainer">
+        <h3>Advice #{"117"}</h3>
+        <p>{`'it is was to sit up and take notice, what's diffuclt is gettinf up and taking action`}</p>
+        <button>
+          <img src={pic} />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
