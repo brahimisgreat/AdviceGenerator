0c51378189fedf08d991d3ed8e15017d1a9d90bd
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-02-10 02:43:48 -0500

    es

--- AdviceGenerator/src/components/Home.jsx
+++ AdviceGenerator/src/components/Home.jsx
@@ -1,19 +1,37 @@
 import "./Home.css";
 import pic from "../assets/images/pause.svg";
-import dice from "../assets/images/icon-dice.svg";
+import Group3 from "../assets/images/Group3.svg";
 import { useEffect, useState } from "react";
+import axios from 'axios'
 
 export default function Home() {
-    useEffect()
+  const [advice, setAdvice] = useState({});
+
+function getData() {
+    axios.get('https://api.adviceslip.com/advice')
+    .then(res => setAdvice(res.data.slip))
+    .catch(err => console.log(err))
+}
+
+
+  useEffect(() => {
+    getData()
+    setInterval(() => {
+        getData()
+        console.log(advice)
+    }, 20000)
+       
+  }, []);
   return (
     <div id="home-container">
       <div id="home-kidtainer">
-        <h3>Advice #{"117"}</h3>
-        <p>{`'it is was to sit up and take notice, what's diffuclt is gettinf up and taking action`}</p>
+        <h3>ADVICE #{advice.id}</h3>
+        <p>{`"${advice.advice}"`}</p>
         <button>
           <img src={pic} />
         </button>
       </div>
+      <img id="dice" src={Group3} />
     </div>
   );
 }
