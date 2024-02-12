1b9d89ae2f4aa2de9aae995d4657ffb7e37c8d7f
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-02-11 22:04:57 -0500

    fdfd

--- AdviceGenerator/src/components/Home.jsx
+++ AdviceGenerator/src/components/Home.jsx
@@ -2,42 +2,37 @@ import "./Home.css";
 import pic from "../assets/images/pause.svg";
 import Group3 from "../assets/images/Group3.svg";
 import { useEffect, useState } from "react";
-import axios from 'axios'
 
 export default function Home() {
   const [advice, setAdvice] = useState({});
-  const [loading, setLoading] = useState(false)
-
-  let pauseNumber = 5000
-
-  function pause () {
-
-  }
-
- async function getData() {
-     setLoading(true)
-     const response= await fetch('https://api.adviceslip.com/advice')
-     const result = await response.json()
-     console.log(loading)
-     setAdvice(result.slip)
-    setLoading(false)
-    console.log(loading)
-}
 
 
-  useEffect(() => {
-    setInterval(() => {
-        getData()  
-        
-        
-    }, pauseNumber)
-  }, []);
+  async function getData() {
+      const response= await fetch('https://api.adviceslip.com/advice')
+      const result = await response.json()
+      setAdvice(result.slip)
+      console.log(advice)
+
+ }
+
+ 
+ 
+ 
+ useEffect(()=> {
+  setInterval(() => {
+     getData()
+ 
+ }, 1000)
+},[])
+
+
+
   return (
     <div id="home-container">
       <div id="home-kidtainer">
         <h3>ADVICE #{advice.id}</h3>
-        {loading ? <p>loading......</p> :  <p>{`"${advice.advice}"`}</p>}
-        <button>
+        <p>{`"${advice.advice}"`}</p>
+        <button >
           <img src={pic} />
         </button>
       </div>
